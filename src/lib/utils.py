ac93e7a23bfe55bdb9d702eabf93f6c42f07550d
tmarsbr <tiagomars233@gmail.com>
2025-10-11 16:17:10 +0000

    criação de libs

---
+++ src/lib/utils.py
@@ -0,0 +1,17 @@
+import json
+from pyspark.sql import types
+
+def table_exists(spark, catalog, database, table):
+    count = (spark.sql(f"SHOW TABLES IN `{catalog}`.`{database}`")
+               .filter(f"database = '{database}' AND tableName = '{table}'")
+               .count())
+    return count == 1
+
+def import_schema(tablename):
+
+    with open(f"{tablename}.json", "r") as open_file:
+        schema_json = json.load(open_file)
+
+    schema_df = types.StructType.fromJson(schema_json)
+
+    return schema_df
